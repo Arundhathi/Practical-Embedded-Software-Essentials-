7fb9825682d4ca710cfc253559d9e0a325ea460c
arundhathi <arundhathi.swami@colorado.edu>
2016-09-20 02:28:10 -0400

    Adding data headers for FRDM

---
+++ PROJECT_1/headers/data_ff.h
@@ -0,0 +1,14 @@
+/* header  file for function called "data"*/
+
+#ifndef data_INCLUDED
+#define data_H_INCLUDED
+
+#include <stdint.h>
+
+	int8_t* my_itoa(int8_t *str, int32_t data, int32_t base);//function converting integral values to strings
+	int32_t my_atoi(int8_t *str);//function converting string ascii inputs to integers
+	void dump_memory(uint8_t *start, uint32_t length);//printing values from memory in hex representation
+	uint32_t big_to_little(uint32_t data);//changes endianness
+	uint32_t little_to_big(uint32_t data);//changes endianness
+#endif
+
