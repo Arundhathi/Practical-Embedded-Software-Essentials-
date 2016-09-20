18eb51299103b30c891cf971dadd421bf27e5ee6
arundhathi <arundhathi.swami@colorado.edu>
2016-09-20 04:47:51 -0400

    Adding furnished header file for FRDM

--- PROJECT_1/headers/project_1_ff.h
+++ PROJECT_1/headers/project_1_ff.h
@@ -1,3 +1,15 @@
+/********************************************
+*   Authors: Arundhathi Swami, Vignesh Jayaram
+*   Date Edited: 19 Sept 2016
+*
+*   File: project_1_ff.h
+*
+*   Description: Header file for final project_report 
+*		 for FRDMKL25z Board
+*                -project_1_report
+*
+********************************************************
+
 /*header for function called "project_1_report"*/
 
 
